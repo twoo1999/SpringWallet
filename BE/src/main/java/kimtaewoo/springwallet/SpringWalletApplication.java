7e9921ff919fddfac8530e4e9845a7603d1e1b48
twoo1999 <twoo19999@gmail.com>
2024-03-25 23:14:45 +0900

    [BE]: Spring 프로젝트 생성

---
+++ BE/src/main/java/kimtaewoo/springwallet/SpringWalletApplication.java
@@ -0,0 +1,13 @@
+package kimtaewoo.springwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWalletApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/kimtaewoo/springwallet/SpringWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package kimtaewoo.springwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
