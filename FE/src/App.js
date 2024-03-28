ebff3490458b06f4f97b2d5db2f82eed686bf69f
twoo1999 <twoo19999@gmail.com>
2024-03-28 16:50:17 +0900

    [FE] :art: header 구현

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import {NavBar} from "./components/Nav/NavBar";
 import styled from "styled-components";
+import {Header} from "./components/header/Header";
 
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+const MainWrapper = styled.div`
+    width: 100%;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar></NavBar>
-    </div>
-  );
+    return (
+        <Wrapper>
+            <NavBar></NavBar>
+            <MainWrapper>
+                <Header></Header>
+            </MainWrapper>
+
+        </Wrapper>
+    );
 }
 
 export default App;
