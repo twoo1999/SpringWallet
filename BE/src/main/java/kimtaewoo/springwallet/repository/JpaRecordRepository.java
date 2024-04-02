45a442e91ea5eeba8735efbdc8770673743d805f
twoo1999 <twoo19999@gmail.com>
2024-04-02 23:26:45 +0900

    [BE] :sparkles: Record Repository 생성
    인터페이스를 만들고 이를 사용해 repository를 생성

---
+++ BE/src/main/java/kimtaewoo/springwallet/repository/JpaRecordRepository.java
@@ -0,0 +1,35 @@
+package kimtaewoo.springwallet.repository;
+
+import jakarta.persistence.EntityManager;
+import kimtaewoo.springwallet.domain.Record;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JpaRecordRepository implements RecordRepository {
+
+
+    private final EntityManager em;
+
+    public JpaRecordRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Record save(Record record) {
+        em.persist(record);
+        return record;
+    }
+
+    @Override
+    public Optional<Record> findById(Long id) {
+        Record re = em.find(Record.class, id);
+        return Optional.ofNullable(re);
+    }
+
+    @Override
+    public List<Record> findAll() {
+        return em.createQuery("select r from Record r", Record.class).getResultList();
+    }
+
+}
