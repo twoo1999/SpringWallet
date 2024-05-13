90db18b7bd94f2ab4485741d2084cb6913ff9e25
twoo1999 <twoo19999@gmail.com>
2024-05-14 03:50:10 +0900

    [FE] :art: 로딩 화면 구현

--- FE/src/components/login/loading.jsx
+++ FE/src/components/login/loading.jsx
@@ -2,32 +2,39 @@ import styled from "styled-components";
 import axios from "axios";
 import {useNavigate, useSearchParams} from "react-router-dom";
 import {useEffect} from "react";
+import '../../common/fonts.css'
 
 const Wrapper = styled.div`
-    width: 100px;
-    height: 100px;
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    padding: 10rem;
+    align-items: center;
+    gap: 5rem;
 `;
 export function Loading(){
     const navigate = useNavigate();
     const [searchParams, setSearchParams] = useSearchParams();
     const code = searchParams.get("code");
-    useEffect(() => {
-        const GoogleLogin = async () => {
-            await axios({
-                method: "GET",
-                url: `http://localhost:8080/auth/google?code=${code}`,
-                withCredentials:true,
-            }).then((res) => {
-                navigate("/login");
-            });
-        };
-        GoogleLogin();
-    }, []);
+    // useEffect(() => {
+    //     const GoogleLogin = async () => {
+    //         await axios({
+    //             method: "GET",
+    //             url: `http://localhost:8080/auth/google?code=${code}`,
+    //             withCredentials:true,
+    //         }).then((res) => {
+    //             navigate("/login");
+    //         });
+    //     };
+    //     GoogleLogin();
+    // }, []);
 
 
     return (
         <Wrapper>
-            로그인중
+            <span className="ExtraBold22">
+                로그인 중...
+            </span>
         </Wrapper>
     );
 }
