7c69207ec0feddb4672991b05d7a68a15278f95d
twoo1999 <twoo19999@gmail.com>
2024-06-05 03:08:37 +0900

    [BE] :sparkles: 커스텀 에러 처리 관련 클래스 구현

--- BE/src/main/java/kimtaewoo/springwallet/exception/CustomExceptionHandler.java
+++ BE/src/main/java/kimtaewoo/springwallet/exception/CustomExceptionHandler.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
-public class CustomExceptionHandler {
-    @ExceptionHandler(CustomException.class)
-    protected ResponseEntity<ErrorResponseEntity> handleCustomException(CustomException e) {
-        return ErrorResponseEntity.toResponseEntity(e.getErrorCode());
-    }
-}
+//@RestControllerAdvice
+//public class CustomExceptionHandler {
+//    @ExceptionHandler(CustomException.class)
+//    protected ResponseEntity<ErrorResponseEntity> handleCustomException(CustomException e) {
+//        return ErrorResponseEntity.toResponseEntity(e.getErrorCode());
+//    }
+//}
