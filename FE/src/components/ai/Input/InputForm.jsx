6bec697d5e56f2b14f57b512c9bfd66376664eee
twoo1999 <twoo19999@gmail.com>
2024-09-09 03:13:01 +0900

    [FE] :sparkles: 버튼 색 변경
     버튼 비활성화 시 lightgray
     버튼 활성화 시 black

--- FE/src/components/ai/Input/InputForm.jsx
+++ FE/src/components/ai/Input/InputForm.jsx
@@ -3,17 +3,33 @@ import {IconButton, InputTable} from "../../../common/commonStyle";
 import {ReactComponent as Analyze} from "../../../assets/Analyze.svg";
 import {TypeInput} from "./TypeInput";
 import {DateInput} from "./DateInput";
+import {useEffect, useState} from "react";
 
 
-export function InputForm(){
+export function InputForm() {
+    const [data, setData] = useState({
+        start: 1,
+        end: 1,
+        type: null,
+    });
+    const [btnAble, setBtnAble] = useState(true);
+
+    useEffect(() => {
+        const flag = Object.values(data).every(val => val != null);
+        if(flag) setBtnAble(false)
+        else setBtnAble(true);
+
+    }, [btnAble]);
+
+
     return (
         <>
             <InputTable>
                 <DateInput type='Start'></DateInput>
                 <DateInput type='End'></DateInput>
-                <TypeInput ></TypeInput>
-                <IconButton>
-                    <Analyze></Analyze>
+                <TypeInput></TypeInput>
+                <IconButton disabled={btnAble}>
+                    <Analyze fill={btnAble ? "lightgray" : "black"}></Analyze>
                 </IconButton>
             </InputTable>
         </>
