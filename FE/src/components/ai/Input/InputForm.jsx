df9cc7dfae9207d0889b28871a819df2d75b7c3b
twoo1999 <twoo19999@gmail.com>
2024-10-13 01:08:46 +0900

    [FE] :sparkles: 남은 토큰 get 및  버튼 활성화
     남은 토큰의 수가 0이면 데이터를 입력해도 버튼을 비활성화

--- FE/src/components/ai/Input/InputForm.jsx
+++ FE/src/components/ai/Input/InputForm.jsx
@@ -4,7 +4,7 @@ import {ReactComponent as Loading} from "../../../assets/loading.svg";
 import {TypeInput} from "./TypeInput";
 import {DateInput} from "./DateInput";
 import {useEffect, useState} from "react";
-import {postApi} from "../../../axiosIntercepter";
+import {getApi, postApi} from "../../../axiosIntercepter";
 
 
 export function InputForm({renewAnalysis}) {
@@ -16,16 +16,23 @@ export function InputForm({renewAnalysis}) {
     const [data, setData] = useState(initValue);
     const [btnAble, setBtnAble] = useState(true);
     const [sse, setSse] = useState(false);
+    const [token, setToken] = useState(0);
     const setDataHandler = (key, val)=>{
         setData((prevState)=>{
             return {...prevState, [key]: val}
         });
     }
 
+    const renewToken = async ()=>{
+        const remainToken = await getApi(`${process.env.REACT_APP_API_URL}/member/token`);
+        setToken(remainToken);
+    }
+
+
     useEffect(() => {
         const flag = Object.values(data).every(val => val != null);
-        if(flag) setBtnAble(false)
-        else setBtnAble(true);
+        if(flag) setBtnAble(false) // 모든 데이터 입력
+        else setBtnAble(true); // 데이터 부족
     }, [data]);
 
     useEffect(async () => {
@@ -46,8 +53,12 @@ export function InputForm({renewAnalysis}) {
             setSse(false);
         }
 
+        await renewToken();
+
+
+
     }, []);
-    const clickPostBtnHandler = ()=>{
+    const clickPostBtnHandler = async ()=>{
         if(new Date(data.start) > new Date(data.end)){
             alert("시작 날짜는 끝 날짜보다 앞서야 합니다. 다시 선택해 주세요.");
             return;
@@ -55,15 +66,17 @@ export function InputForm({renewAnalysis}) {
 
         const s= new EventSource(`${process.env.REACT_APP_API_URL}/ai/emitter`, { withCredentials: true });
         setSse(true);
-        s.addEventListener('data', (e)=>{
+        s.addEventListener('data', async (e)=>{
             alert("분석이 끝났습니다.");
             renewAnalysis();
             s.close();
             setSse(false);
+            await renewToken();
         })
 
         postApi(`${process.env.REACT_APP_API_URL}/ai/gemini`, data); //
         setData(initValue);
+
 
 
     }
@@ -78,14 +91,14 @@ export function InputForm({renewAnalysis}) {
                 <DateInput type='End' value={data.end} setDataHandler={setDataHandler}></DateInput>
                 <TypeInput value={data.type} setDataHandler={setDataHandler}></TypeInput>
                 <FlexRowDivByGap gap="2rem">
-                    <IconButton disabled={btnAble}>
+                    <IconButton disabled={btnAble || token === 0}>
                         {
-                            !sse ? <Analyze onClick={clickPostBtnHandler}  fill={btnAble ? "lightgray" : "black"}></Analyze>
+                            !sse ? <Analyze onClick={clickPostBtnHandler}  fill={btnAble||token===0 ? "lightgray" : "black"}></Analyze>
                                 : <Loading></Loading>
                         }
 
                     </IconButton>
-                    <span className="Bold16">(1/5)</span>
+                    <span className="Bold16">{token}/5</span>
                 </FlexRowDivByGap>
 
             </InputTable>
