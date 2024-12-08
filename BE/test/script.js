a86cb0b76723c8f45f392dc8392d1f868c0a318f
twoo1999 <twoo19999@gmail.com>
2024-12-09 04:22:13 +0900

    [BE] :test_tube: k6 테스트 코드 수정
    setup을 통한 토큰 생성
    
    VU code를 통해 테스트 실행

---
+++ BE/test/script.js
@@ -0,0 +1,39 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import {encode} from './util.js'
+import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+
+export const options = {
+  vus: 10,
+  duration: '5s',
+};
+
+export function setup(){
+  const tokens = Array(10).fill().map((v, i)=>{
+    const data = {
+      "id": `${uuidv4()}`,
+      "email" : `test${i}@test.com`,
+      "name": `test${i}`,
+      "role": "GUEST"
+    }
+    return encode(data, "testaccess");
+
+  });
+  return {tokens};
+}
+
+
+export default function({tokens}) {
+  const token = tokens[__VU-1];
+
+
+  const response = http.get("http://localhost:8080/test/api/record", {
+    headers: {
+      'Cookie' : `AccessToken=${token};`
+    }
+  });
+  check(response, {
+    "is status 200" :(r) => r.status === 200,
+  })
+  sleep(1);
+}
